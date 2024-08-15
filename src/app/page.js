870a0161264c1c68503f1c3cd97a892879cc79c7
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-08-15 19:40:45 -0400

    Finishes the featured products section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
+import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Slider />
+      <div className="mt-24 px-4 md:px-8">
+        <h1 className="text-2xl">Featured Products</h1>
+        <ProductList />
+      </div>
     </div>
   );
 }

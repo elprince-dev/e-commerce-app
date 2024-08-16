6c3ee999d3dccfb5b7bd58d4f0afc9ba959fb1d4
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-08-15 20:59:47 -0400

    Codes category list component

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import CategoryList from "@/components/CategoryList";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
 
@@ -7,6 +8,14 @@ export default function Home() {
       <Slider />
       <div className="mt-24 px-4 md:px-8">
         <h1 className="text-2xl">Featured Products</h1>
+        <ProductList />
+      </div>
+      <div className="mt-24">
+        <h1 className="text-2xl px-4 md:px-8 mb-12">Categoris</h1>
+        <CategoryList />
+      </div>
+      <div className="mt-24 px-4 md:px-8">
+        <h1 className="text-2xl">New Products</h1>
         <ProductList />
       </div>
     </div>

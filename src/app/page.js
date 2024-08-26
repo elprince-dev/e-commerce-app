6c01c23e181e45a8b23cbdce80e549f7f3375a4a
Mohamed Hussein <mohamedmhussein@protonmail.com>
2024-08-26 00:19:54 -0400

    Sets up fetching featured products

--- src/app/page.js
+++ src/app/page.js
@@ -32,7 +32,7 @@ export default async function Home() {
       </div>
       <div className="mt-24 px-4 md:px-8">
         <h1 className="text-2xl">New Products</h1>
-        <ProductList />
+        {/* <ProductList /> */}
       </div>
     </div>
   );
